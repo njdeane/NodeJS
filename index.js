ab052583bd676f375fc3105d6f68091ff13ccded
njdeane <njdeane7@gmail.com>
2020-11-04 15:22:11 +1000

    Added 2 external packages from 'NPM' and incorporated them into the program as dependancies to be able to print out hero/villain names to the terminal

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const fs = require("fs");
+// this is using an internal nodeJS module 
+// const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
-// running this file in terminal (node index.html) will copy file1.txt and make a duplicate called file2.txt 
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+
+// This line incorporates an external packate - the NPM superhero module into the project
+var superheroes = require("superheroes");
+var superVillains = require("supervillains");
+
+// These lines use a function from the NPM package (info on NPM superhero package page), stores it in a variable that we can then run from terminal to get random superhero names.
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = superVillains.random();
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);
+
+// the console.logs have to be at the bottom, before I had the console.log for hero and vilain underneath each so the code would execute the console.log to the terminal before being able to get to the next variable. having the .logs at the bottom allows both hero and villain to be printed to the terminal.
